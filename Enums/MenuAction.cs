f0520dec9f6b6e4584a25b4c00e912a59455382a
Kiril <remisovkiril@gmail.com>
2026-03-13 13:33:52 +0200

    Added Repository, Service, Manager menu and OrderService with transaction

---
+++ Enums/MenuAction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Enums
+{
+    internal class MenuAction
+    {
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Repositories
+{
+    internal class ProductRepository
+    {
+    }
+}
